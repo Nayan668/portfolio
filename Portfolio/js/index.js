34a36e8c6c265a872f40b7c8c67620c3fee4275d
Monjurul Imam Nayan <74012390+Nayan668@users.noreply.github.com>
2026-01-29 04:21:48 +0600

    Add files via upload

--- Portfolio/js/index.js
+++ Portfolio/js/index.js
@@ -16,3 +16,39 @@ function updateDateTime() {
 // Protiti second-e update hobe
 setInterval(updateDateTime, 1000);
 updateDateTime(); // Initial call
+
+
+const textElement = document.getElementById("typing-text");
+const texts = ["Nayan | Portfolio", "Hi, I'm Monjurul Imam Nayan 👋"];
+let speed = 100; // Type speed
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    const currentText = texts[textIndex];
+    
+    if (isDeleting) {
+        textElement.innerText = currentText.substring(0, charIndex - 1);
+        charIndex--;
+        speed = 50; // Deleting speed
+    } else {
+        textElement.innerText = currentText.substring(0, charIndex + 1);
+        charIndex++;
+        speed = 100; // Normal typing speed
+    }
+
+    if (!isDeleting && charIndex === currentText.length) {
+        isDeleting = true;
+        speed = 2000; // Pause at the end
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+        speed = 500;
+    }
+
+    setTimeout(typeEffect, speed);
+}
+
+// Start the effect
+document.addEventListener("DOMContentLoaded", typeEffect);
